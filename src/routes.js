50e135a023aabb046a8fb25f86bc0ce5b5384181
Felipe Rêgo <feliperrego@gmail.com>
2018-10-28 17:29:53 -0300

    Terms page component

--- src/routes.js
+++ src/routes.js
@@ -6,29 +6,13 @@ import AppRoute from "./components/AppRoute";
 import Home from './pages/Home';
 import About from "./pages/About";
 import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms/Terms";
 
 const routes = [
-	{
-		path: '/',
-		component: Home,
-		layout: PublicLayout,
-		private: false,
-		exact: true
-	},
-	{
-		path: '/about',
-		component: About,
-		layout: PublicLayout,
-		private: false,
-		exact: true
-	},
-	{
-		path: '/privacy',
-		component: Privacy,
-		layout: PublicLayout,
-		private: false,
-		exact: true
-	},
+	{ path: '/', component: Home, layout: PublicLayout, private: false, exact: true },
+	{ path: '/about', component: About, layout: PublicLayout, private: false, exact: true },
+	{	path: '/privacy', component: Privacy, layout: PublicLayout, private: false, exact: true },
+	{	path: '/terms', component: Terms, layout: PublicLayout, private: false, exact: true },
 ];
 
 class Routes extends Component {
