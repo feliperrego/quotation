83dbbe422bb35e5f867955e53bfa59ab69f561d8
Felipe Rêgo <feliperrego@gmail.com>
2018-10-28 16:25:54 -0300

    Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class Footer extends Component {
+	render() {
+		return (
+			<footer>
+				<ul>
+					<li>
+						<Link to="about">about</Link>
+					</li>
+					<li>
+						<Link to="privacy">privacy</Link>
+					</li>
+					<li>
+						<Link to="terms">terms</Link>
+					</li>
+				</ul>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
--- src/components/Layout/PublicLayout.js
+++ src/components/Layout/PublicLayout.js
@@ -1,13 +1,15 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Header from "../Header";
+import Footer from "../Footer";
 
 export class PublicLayout extends Component {
 	render() {
 		return (
 			<Fragment>
-				<Header/>
+				<Header />
 				{this.props.children}
+				<Footer />
 			</Fragment>
 		);
 	}
