06faebdc13bcc0cef7ea74b4b5d9942a871d23e0
Felipe Rêgo <feliperrego@gmail.com>
2018-10-29 18:57:51 -0300

    Add link to home on another pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-const Header = (props) => {
+const Header = () => {
+
+	const title = 'quotation';
+
 	return (
 		<header>
-			<h1>quotation</h1>
+			<h1>{
+				window.location.pathname === '/'
+					? title
+					: <Link to={"/"}>{title}</Link>
+			}</h1>
 		</header>
 	);
 };
